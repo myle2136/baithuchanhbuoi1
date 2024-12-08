b6c9b7789782f1056bd6b4c36c16ebcde234cafb
myle2136 <lemy2136@gmail.com>
2024-12-08 13:04:53 +0700

    Add files via upload

---
+++ Baithuchanhbuoi1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+	printf ("\nMSSV:110124124");
+	printf ("\nHo Ten: LE THI DIEM MY");
+	printf ("\nNam sinh:2006");
+	printf ("\nDiem xet tuyen:23.79");
+	printf ("\nLop:DA24TTA");
+	return 0;
+}
